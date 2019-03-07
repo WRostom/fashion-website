9af9ef9337369e4609bd06bc19c418b8b77e97d7
Waleed Rostom <waleedrostom@gmail.com>
2019-03-08 00:30:26 +0200

    version 0.1

---
+++ palette.js
@@ -0,0 +1,26 @@
+// import Vibrant = require('node-vibrant');
+
+let root = document.documentElement;
+let colorCircles = document.querySelectorAll('.color');
+let heroImage = document.getElementById('heroimage');
+let imagesrc = heroImage.src;
+let colorscheme = [];
+// let initialCircle = document.querySelector('.colorBright');
+Vibrant.from(imagesrc)
+    .getPalette()
+    .then(palette => {
+        console.log(palette);
+        let max = { objectHex: '', maxvalue: 0 };
+        let counter = 0;
+        for (let palettechoice in palette) {
+            colorCircles[counter].style.backgroundColor = palette[palettechoice].getHex();
+            if (palette[palettechoice].population > max.maxvalue) {
+                max = {
+                    objectHex: palette[palettechoice].hex,
+                    maxvalue: palette[palettechoice].population,
+                };
+            }
+            counter++;
+        }
+        root.style.setProperty('--main-color', max.objectHex);
+    });
